8178bde4bd2af4b6447f5540fb0c2d395c3ec02c
Ivison <Ivisonk0@gmail.com>
2021-12-07 09:54:20 -0300

    add button in the second activity to close it

--- app/src/main/java/com/ivisondsb/activities/SecondActivity.kt
+++ app/src/main/java/com/ivisondsb/activities/SecondActivity.kt
@@ -2,10 +2,22 @@ package com.ivisondsb.activities
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import kotlinx.android.synthetic.main.activity_main.*
 
 class SecondActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_second)
+    }
+
+    override fun onResume() {
+        super.onResume()
+
+        Log.i("LOG", "onResume() Second Activity" + System.currentTimeMillis())
+
+        button.setOnClickListener {
+            finish()
+        }
     }
 }

8a8c2ea104703b73561ad39545fb87ac8e9609c2
Ivison <Ivisonk0@gmail.com>
2021-12-07 09:40:23 -0300

    add button in main activity to access second activity

--- app/src/main/java/com/ivisondsb/activities/MainActivity.kt
+++ app/src/main/java/com/ivisondsb/activities/MainActivity.kt
@@ -1,11 +1,32 @@
 package com.ivisondsb.activities
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        Log.i("LOG", "onCreate()" + System.currentTimeMillis())
+    }
+
+    override fun onStart() {
+        super.onStart()
+
+        Log.i("LOG", "onStart()" + System.currentTimeMillis())
+    }
+
+    override fun onResume() {
+        super.onResume()
+
+        Log.i("LOG", "onResume()" + System.currentTimeMillis())
+
+        button.setOnClickListener {
+            startActivity(Intent(this, SecondActivity::class.java))
+        }
     }
 }

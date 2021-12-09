4ce8a3cdf5c2f7d74377cb484b15088c5934d49b
Ivison <Ivisonk0@gmail.com>
2021-12-09 17:47:29 -0300

    return data from second activity to main

--- app/src/main/java/com/ivisondsb/activities/MainActivity.kt
+++ app/src/main/java/com/ivisondsb/activities/MainActivity.kt
@@ -1,44 +1,76 @@
 package com.ivisondsb.activities
 
+import android.app.Activity
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
-import kotlinx.android.synthetic.main.activity_main.*
+import android.widget.Toast
+import androidx.activity.result.ActivityResultLauncher
+import androidx.activity.result.contract.ActivityResultContracts
+import kotlinx.android.synthetic.main.activity_main.button
+
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var resultLauncher: ActivityResultLauncher<Intent>
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        Log.i("LOG", "onCreate()" + System.currentTimeMillis())
+        Log.i("LOG", "onCreate()")
+
+        resultLauncher =
+            registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result ->
+                if (result.resultCode == Activity.RESULT_OK) {
+                    val data: Intent? = result.data?.apply {
+                        Toast.makeText(
+                            this@MainActivity,
+                            getStringExtra("RETURN"),
+                            Toast.LENGTH_SHORT
+                        ).show()
+                    }
+
+                }
+            }
     }
 
     override fun onStart() {
         super.onStart()
 
-        Log.i("LOG", "onStart()" + System.currentTimeMillis())
+        Log.i("LOG", "onStart()")
     }
 
     override fun onResume() {
         super.onResume()
 
-        Log.i("LOG", "onResume()" + System.currentTimeMillis())
+        Log.i("LOG", "onResume()")
 
         button.setOnClickListener {
-            startActivity(Intent(this, SecondActivity::class.java))
+
+            val intent = Intent(this, SecondActivity::class.java)
+            intent.putExtra("TEXT_VALUE", "Name")
+            resultLauncher.launch(intent)
         }
+    }
+
+    override fun onPause() {
+        super.onPause()
+
+        Log.i("LOG", "onResume()")
+
     }
 
     override fun onStop() {
         super.onStop()
 
-        Log.i("LOG", "onResume()" + System.currentTimeMillis())
+        Log.i("LOG", "onResume()")
     }
 
     override fun onDestroy() {
         super.onDestroy()
 
-        Log.i("LOG", "onResume()" + System.currentTimeMillis())
+        Log.i("LOG", "onResume()")
     }
 }
--- app/src/main/java/com/ivisondsb/activities/SecondActivity.kt
+++ app/src/main/java/com/ivisondsb/activities/SecondActivity.kt
@@ -1,9 +1,11 @@
 package com.ivisondsb.activities
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
-import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.activity_main.button
+import kotlinx.android.synthetic.main.activity_second.*
 
 class SecondActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -11,12 +13,22 @@ class SecondActivity : AppCompatActivity() {
         setContentView(R.layout.activity_second)
     }
 
+    override fun onStart() {
+        super.onStart()
+        text.text = intent.getStringExtra("TEXT_VALUE")
+        Log.i("LOG", "onStart()")
+    }
+
     override fun onResume() {
         super.onResume()
 
-        Log.i("LOG", "onResume() Second Activity" + System.currentTimeMillis())
+        Log.i("LOG", "onResume() Second Activity")
 
         button.setOnClickListener {
+
+            val data = Intent()
+            data.putExtra("RETURN", edtt.text.toString())
+            setResult(RESULT_OK, data)
             finish()
         }
     }
